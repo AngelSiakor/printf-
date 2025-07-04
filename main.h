8a1d2f6fe3505280c31b936635482b7c0f90ca2c
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-07-04 12:04:15 +0000

    this the hearder file for the priject

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H  // Include guard start
+#define MAIN_H
+#include <stdlib.h>
+// Function prototypes
+int _printf(char *format, ...);
+int _putchar(char c);
+int print_num(int num);
+int str_len(char *s);
+int print_float(double num, int precision);
+int print_reverse(char *s);
+int print_binary(unsigned int num);
+int print_hex(unsigned int num);
+int print_unsigned(unsigned int num);
+#endif 
