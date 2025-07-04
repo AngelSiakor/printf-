4427119d39e754445b7cc6c680bf473a386937fe
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-07-04 14:32:20 +0000

    this function is to rev a string from left to right guys

--- printf.c
+++ printf.c
@@ -7,4 +7,16 @@ int _putchar(char c)
 {
         return write(1, &c, 1);
 }
+int print_reverse(char *s)
+{
+    int len = 0;
+   int  count = 0;
 
+    while (s[len])
+        len++;
+
+    for (int i = len - 1; i >= 0; i--)
+        count += _putchar(s[i]);
+
+    return count;
+}
