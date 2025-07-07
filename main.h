0ab3dd7ebc3baa1b56e16c8b8b3f08199ef630c4
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-07-07 11:34:02 +0000

    this the main function for the functuin

--- main.h
+++ main.h
@@ -8,7 +8,8 @@ int print_num(int num);
 int str_len(char *s);
 int print_float(double num, int precision);
 int print_reverse(char *s);
+int octal_num(int num);
+int print_unsigned(unsigned int num);
 int print_binary(unsigned int num);
 int print_hex(unsigned int num);
-int print_unsigned(unsigned int num);
 #endif 
